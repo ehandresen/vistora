7e9db5cc72c97944a44f80418db0ec962f5b2f58
Eirik Andresen <eirik.horgen.andresen@gmail.com>
2025-03-26 23:17:47 +0100

    add footer

---
+++ components/footer.tsx
@@ -0,0 +1,13 @@
+import { APP_NAME } from "@/lib/constants";
+
+function Footer() {
+  const currentYear = new Date().getFullYear();
+  return (
+    <footer className="border-t border-gray-300">
+      <div className="p-5 flex-center">
+        {currentYear} {APP_NAME}. All Rights Reserved.
+      </div>
+    </footer>
+  );
+}
+export default Footer;
