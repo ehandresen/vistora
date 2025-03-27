9190aa3c02dc8433450134bb7fe2c7676b621fa6
Eirik Andresen <eirik.horgen.andresen@gmail.com>
2025-03-27 22:44:43 +0100

    fetch products from db

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,14 +1,12 @@
 import ProductList from "@/components/shared/product/product-list";
-import sampleData from "@/db/sample-data";
+import { getLatestProducts } from "@/lib/actions/product.actions";
 
-function HomePage() {
+async function HomePage() {
+  const latestProducts = await getLatestProducts();
+
   return (
     <>
-      <ProductList
-        data={sampleData.products}
-        title="Newest arrivals"
-        limit={4}
-      />
+      <ProductList data={latestProducts} title="Newest arrivals" limit={4} />
     </>
   );
 }
