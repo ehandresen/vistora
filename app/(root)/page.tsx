0350960c7e28ab24e71cb63b79d2704d0dc2bda4
Eirik Andresen <eirik.horgen.andresen@gmail.com>
2025-03-27 17:46:32 +0100

    add sample product data, display name for now

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,15 @@
+import ProductList from "@/components/shared/product/product-list";
+import sampleData from "@/db/sample-data";
+
 function HomePage() {
-  return <div>Vistora</div>;
+  return (
+    <>
+      <ProductList
+        data={sampleData.products}
+        title="Newest arrivals"
+        limit={4}
+      />
+    </>
+  );
 }
 export default HomePage;
