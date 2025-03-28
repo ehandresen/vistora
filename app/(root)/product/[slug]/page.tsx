78d002e0a8de5b5a70409788a02ed27b7d3f5e84
Eirik Andresen <eirik.horgen.andresen@gmail.com>
2025-03-28 22:59:13 +0100

    add product-images component

--- app/(root)/product/[slug]/page.tsx
+++ app/(root)/product/[slug]/page.tsx
@@ -4,6 +4,7 @@ import { Card, CardContent } from "@/components/ui/card";
 import { getProductBySlug } from "@/lib/actions/product.actions";
 import { notFound } from "next/navigation";
 import ProductPrice from "@/components/shared/product/product-price";
+import ProductImages from "@/components/shared/product/product-images";
 
 async function ProductDetailsPage(props: {
   params: Promise<{ slug: string }>;
@@ -18,7 +19,9 @@ async function ProductDetailsPage(props: {
       <section>
         <div className="grid grid-cols-1 md:grid-cols-5">
           {/* images column */}
-          <div className="col-span-2">{/* images component */}</div>
+          <div className="col-span-2">
+            <ProductImages images={product.images} />
+          </div>
           {/* details column */}
           <div className="col-span-2 p-5">
             <div className="flex flex-col gap-6">
