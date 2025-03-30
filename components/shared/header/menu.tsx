634fe9f4b21a54dc9dda6e2dc85d32be2421649e
Eirik Andresen <eirik.horgen.andresen@gmail.com>
2025-03-30 23:18:11 +0200

    add user-button w/ sign out functinoality

--- components/shared/header/menu.tsx
+++ components/shared/header/menu.tsx
@@ -1,13 +1,14 @@
 import ModeToggle from "./mode-toggle";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
-import { EllipsisVertical, ShoppingCart, UserIcon } from "lucide-react";
+import { EllipsisVertical, ShoppingCart } from "lucide-react";
 import {
   Sheet,
   SheetContent,
   SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet";
+import UserButton from "./user-button";
 
 function Menu() {
   return (
@@ -19,11 +20,7 @@ function Menu() {
             <ShoppingCart /> Cart
           </Link>
         </Button>
-        <Button asChild>
-          <Link href="/sign-in">
-            <UserIcon /> Sign In
-          </Link>
-        </Button>
+        <UserButton />
       </nav>
       <nav className="md:hidden">
         <Sheet>
@@ -39,12 +36,7 @@ function Menu() {
                 Cart
               </Link>
             </Button>
-            <Button asChild>
-              <Link href="/sign-in">
-                <UserIcon />
-                Sign In
-              </Link>
-            </Button>
+            <UserButton />
             {/* maybe sheet description is required */}
             {/* <SheetDescription></SheetDescription>  */}
           </SheetContent>
--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -11,6 +11,6 @@ export const LATEST_PRODUCTS_LIMIT =
   Number(process.env.LATEST_PRODUCTS_LIMIT) || 4;
 
 export const signInDefaultValues = {
-  email: "",
-  password: "",
+  email: "admin@example.com",
+  password: "123456",
 };
