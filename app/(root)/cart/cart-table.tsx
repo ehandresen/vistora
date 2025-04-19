0c8ce4b113dbf6930b60de47d8f68e23d2d18a62
Eirik Andresen <eirik.horgen.andresen@gmail.com>
2025-04-19 09:35:44 +0200

    add cartpage and a table using shadcn

---
+++ app/(root)/cart/cart-table.tsx
@@ -0,0 +1,130 @@
+"use client";
+
+import { ArrowRight, Loader, Minus, Plus } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useState, useTransition } from "react";
+import { toast } from "sonner";
+
+import { Button } from "@/components/ui/button";
+import {
+    Table, TableBody, TableCell, TableHead, TableHeader, TableRow
+} from "@/components/ui/table";
+import { addItemToCart, removeItemFromCart } from "@/lib/actions/cart.actions";
+import { Cart } from "@/types";
+
+function CartTable({ cart }: { cart?: Cart }) {
+  const router = useRouter();
+
+  const [isPending, startTransition] = useTransition();
+  const [updatingItemId, setUpdatingItemId] = useState<string | null>(null);
+  const [currentAction, setCurrentAction] = useState<"add" | "remove" | null>(
+    null
+  );
+
+  return (
+    <>
+      <h1 className="py-4 h2-bold">Shopping Cart</h1>
+      {!cart || cart.items.length === 0 ? (
+        <div>
+            Cart is empty. <Link href="/">Go Shopping</Link>
+        </div>
+      ) : (
+        <div className="grid md:grid-cols-4 md:gap-5">
+          <div className="overflow-x-auto md:col-span-3">
+            <Table>
+              <TableHeader>
+                <TableRow>
+                  <TableHead>Item</TableHead>
+                  <TableHead className="text-center">Quantity</TableHead>
+                  <TableHead className="text-right">Price</TableHead>
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+                {cart.items.map((item) => (
+                  <TableRow key={item.slug}>
+                    <TableCell>
+                      <Link
+                        href={`/product/${item.slug}`}
+                        className="flex items-center"
+                      >
+                        <Image
+                          src={item.image}
+                          alt={item.name}
+                          width={50}
+                          height={50}
+                        />
+                        <span className="px-2">{item.name}</span>
+                      </Link>
+                    </TableCell>
+                    <TableCell className="flex-center gap-2">
+                      {/*  Remove btn */}
+                      <Button
+                        disabled={isPending}
+                        variant="outline"
+                        type="button"
+                        onClick={() => {
+                          setUpdatingItemId(item.productId);
+                          setCurrentAction("remove");
+
+                          startTransition(async () => {
+                            const res = await removeItemFromCart(
+                              item.productId
+                            );
+
+                            if (!res.success) {
+                              toast.error(res.message);
+                            }
+                          });
+                        }}
+                      >
+                        {isPending &&
+                        updatingItemId === item.productId &&
+                        currentAction === "remove" ? (
+                          <Loader className="w-4 h-4 animate-spin" />
+                        ) : (
+                          <Minus className="w-4 h-4" />
+                        )}
+                      </Button>
+                      <span>{item.qty}</span>
+
+                      {/* Add btn */}
+                      <Button
+                        disabled={isPending}
+                        variant="outline"
+                        type="button"
+                        onClick={() => {
+                          setUpdatingItemId(item.productId);
+                          setCurrentAction("add");
+
+                          startTransition(async () => {
+                            const res = await addItemToCart(item);
+
+                            if (!res.success) {
+                              toast.error(res.message);
+                            }
+                          });
+                        }}
+                      >
+                        {isPending &&
+                        updatingItemId === item.productId &&
+                        currentAction === "add" ? (
+                          <Loader className="w-4 h-4 animate-spin" />
+                        ) : (
+                          <Plus className="w-4 h-4" />
+                        )}
+                      </Button>
+                    </TableCell>
+                    <TableCell className="text-right">${item.price}</TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </div>
+        </div>
+      )}
+    </>
+  );
+}
+export default CartTable;
