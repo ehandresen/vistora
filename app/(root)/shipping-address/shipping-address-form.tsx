02cb06bd75ce766eda87cab2f4b178d9f26e654c
Eirik Andresen <eirik.horgen.andresen@gmail.com>
2025-04-21 06:24:31 +0200

    add payment-method page with form

--- app/(root)/shipping-address/shipping-address-form.tsx
+++ app/(root)/shipping-address/shipping-address-form.tsx
@@ -9,13 +9,7 @@ import { z } from "zod";
 
 import { Button } from "@/components/ui/button";
 import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
+    Form, FormControl, FormField, FormItem, FormLabel, FormMessage
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { updateUserAddress } from "@/lib/actions/user.actions";
