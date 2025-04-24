bb0fb4e865ec11c7cc8e3e8dff0da3884a53d1f6
Eirik Andresen <eirik.horgen.andresen@gmail.com>
2025-04-24 21:04:51 +0200

    add carousel for banners on homepage

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,16 @@
+import ProductCarousel from "@/components/shared/product/product-carousel";
 import ProductList from "@/components/shared/product/product-list";
-import { getLatestProducts } from "@/lib/actions/product.actions";
+import { getFeaturedProducts, getLatestProducts } from "@/lib/actions/product.actions";
 
 async function HomePage() {
   const latestProducts = await getLatestProducts();
+  const featuredProfucts = await getFeaturedProducts();
 
   return (
     <>
+      {featuredProfucts.length > 0 && (
+        <ProductCarousel data={featuredProfucts} />
+      )}
       <ProductList data={latestProducts} title="Newest arrivals" limit={4} />
     </>
   );
--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -1,6 +1,9 @@
 import Image from "next/image";
 import Link from "next/link";
+
 import { APP_NAME } from "@/lib/constants";
+
+import CategoryDrawer from "./category-drawer";
 import Menu from "./menu";
 
 function Header() {
@@ -8,7 +11,8 @@ function Header() {
     <header className="w-full border-b border-gray-300 h-16 flex ">
       <div className="wrapper flex-between">
         <div className="flex-start">
-          <Link href="/" className="flex-start">
+          <CategoryDrawer />
+          <Link href="/" className="flex-start ml-4">
             <Image
               src="/images/logo.svg"
               alt={`${APP_NAME} logo`}
