87b465017391ea8093eb8dd2caf104665483378b
Eirik Andresen <eirik.horgen.andresen@gmail.com>
2025-04-25 10:32:18 +0200

    add promo section on homepage

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,5 @@
+import DealCountdown from "@/components/deal-countdown";
+import IconBoxes from "@/components/icon-boxes";
 import ProductCarousel from "@/components/shared/product/product-carousel";
 import ProductList from "@/components/shared/product/product-list";
 import ViewAllProductsButton from "@/components/view-all-products-button";
@@ -14,6 +16,8 @@ async function HomePage() {
       )}
       <ProductList data={latestProducts} title="Newest arrivals" limit={4} />
       <ViewAllProductsButton />
+      <DealCountdown />
+      <IconBoxes />
     </>
   );
 }
---
+++ components/deal-countdown.tsx
@@ -0,0 +1,132 @@
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+
+import { Button } from "./ui/button";
+
+const TARGET_DATE = new Date(Date.now() + 3 * 24 * 60 * 60 * 1000); // 3 days from now
+
+// Function to calculate the time remaining
+function calculateTimeRemaining(targetDate: Date) {
+  const currentTime = new Date();
+  const timeDifference = Math.max(Number(targetDate) - Number(currentTime), 0);
+  return {
+    days: Math.floor(timeDifference / (1000 * 60 * 60 * 24)),
+    hours: Math.floor(
+      (timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    ),
+    minutes: Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60)),
+    seconds: Math.floor((timeDifference % (1000 * 60)) / 1000),
+  };
+}
+
+function DealCountdown() {
+  const [time, setTime] = useState<ReturnType<typeof calculateTimeRemaining>>();
+
+  useEffect(() => {
+    // Calculate initial time
+    setTime(calculateTimeRemaining(TARGET_DATE));
+
+    const timerInterval = setInterval(() => {
+      const newTime = calculateTimeRemaining(TARGET_DATE);
+      setTime(newTime);
+
+      if (
+        newTime.days === 0 &&
+        newTime.hours === 0 &&
+        newTime.minutes === 0 &&
+        newTime.seconds === 0
+      ) {
+        clearInterval(timerInterval);
+      }
+
+      return () => clearInterval(timerInterval);
+    }, 1000);
+  }, []);
+
+  if (!time) {
+    return (
+      <section className="grid grid-cols-1 md:grid-cols-2 my-20">
+        <div className="flex flex-col gap-2 justify-center">
+          <h3 className="text-3xl font-bold">Loading Countdown...</h3>
+        </div>
+      </section>
+    );
+  }
+
+  if (
+    time.days === 0 &&
+    time.hours === 0 &&
+    time.minutes === 0 &&
+    time.seconds === 0
+  ) {
+    return (
+      <section className="grid grid-cols-1 md:grid-cols-2 my-20">
+        <div className="flex flex-col gap-2 justify-center">
+          <h3 className="text-3xl font-bold">Offer Expired</h3>
+          <p>
+            This offer has ended. But we’ve got more great deals waiting for
+            you!
+          </p>
+
+          <div className="text-center">
+            <Button asChild>
+              <Link href="/search">View Products</Link>
+            </Button>
+          </div>
+        </div>
+        <div className="flex justify-center">
+          <Image
+            src="/images/promo.jpg"
+            alt="promotion"
+            width={300}
+            height={200}
+          />
+        </div>
+      </section>
+    );
+  }
+
+  return (
+    <section className="grid grid-cols-1 md:grid-cols-2 my-20">
+      <div className="flex flex-col gap-2 justify-center">
+        <h3 className="text-3xl font-bold">Limited Time Offer</h3>
+        <p>
+          Discover this month’s exclusive offer. Great value, limited time! 🛍️
+        </p>
+        <ul className="grid grid-cols-4">
+          <StatBox label="Days" value={time.days} />
+          <StatBox label="Hours" value={time.hours} />
+          <StatBox label="Minutes" value={time.minutes} />
+          <StatBox label="Seconds" value={time.seconds} />
+        </ul>
+        <div className="text-center">
+          <Button asChild>
+            <Link href="/search">View Products</Link>
+          </Button>
+        </div>
+      </div>
+      <div className="flex justify-center">
+        <Image
+          src="/images/promo.jpg"
+          alt="promotion"
+          width={300}
+          height={200}
+        />
+      </div>
+    </section>
+  );
+}
+
+function StatBox({ label, value }: { label: string; value: number }) {
+  return (
+    <li className="p-4 w-full text-center">
+      <p className="text-3xl font-bold">{value}</p>
+      <p>{label}</p>
+    </li>
+  );
+}
+
+export default DealCountdown;
